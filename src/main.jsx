33dea2871b75b4ce6b6522a72fe7e9b48b205f5f
HanniaDeLuna <haalortegade@ittepic.edu.mx>
2025-02-25 23:13:28 -0600

    Segundo commit 6.2 App principal(main)

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import './share/css/allPages.css'
+import AppAllModules from './AppAllModules.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  
+   {/*<App />*/ } 
+    <AppAllModules/> {/*6.2 App principal*/ }
   </StrictMode>,
 )
